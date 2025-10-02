675632cee21a2ebad8d63c9ac82903773ed7b48b
Zahin <137389784+ZahinZee@users.noreply.github.com>
2025-10-02 19:52:54 +0600

    Feat: Added More Small JS projects

---
+++ JavaScript/build-a-sentence-maker.js
@@ -0,0 +1,24 @@
+let adjective = "Pretty";
+let noun ="Flowers";
+let verb = "blooming";
+let place = "garden";
+let adjective2 = "aromatic";
+let noun2 = "pests";
+
+let firstStory;
+
+firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`First story: ${firstStory}`);
+
+let secondStory;
+adjective = "brave";
+noun = "dragon";
+verb = "breathing";
+place = "mountain";
+adjective2 = "angry";
+noun2 = "human";
+
+secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+
+console.log(`Second  story: ${secondStory}`);
---
+++ JavaScript/lab-javascript-trivia-bot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Kodie";
+let botLocation = "Internet";
+let favoriteLanguage = "Python";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+ 
+let codingFact = "In " + favoriteLanguage + ", the name actually comes from Monty Python’s Flying Circus (a British comedy show), not the snake.";
+
+console.log(codingFact); 
+
+codingFact = "The very first version of " + favoriteLanguage + "was released in 1991 by Guido van Rossum, making it older than Google!"; 
+
+console.log(codingFact); 
+
+codingFact = favoriteLanguage  + " is the only programming language that can run natively in all modern web browsers.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName+" from " + botLocation + "."); 
